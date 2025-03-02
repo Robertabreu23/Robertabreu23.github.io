85a24e997a249051afaa2e06d31ddcdd4812e4c7
Robertabreu23 <abreudelgado2@gmail.com>
2025-03-01 22:38:48 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('inicioBtn').addEventListener('click', function() {
+    document.getElementById('portada').style.display = 'none';
+    document.getElementById('mainHeader').style.display = 'block';
+});
